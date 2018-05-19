35d18689385d040ef88eec08d6355f322c29f70c
Michael Lynch <git@mtlynch.io>
2018-05-19 14:46:52 -0400

    Adding mailing list signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import {
   MatButtonModule,
   MatCardModule,
+  MatExpansionModule,
   MatFormFieldModule,
   MatInputModule,
   MatListModule,
@@ -20,11 +21,13 @@ import { ParserService } from './_services/parser.service';
 
 // Public pages
 import { HomePageComponent } from './public/home-page/home-page.component';
+import { MailingListComponent } from './_components/mailing-list/mailing-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+    MailingListComponent,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -33,6 +36,7 @@ import { HomePageComponent } from './public/home-page/home-page.component';
     HttpClientModule,
     MatButtonModule,
     MatCardModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatInputModule,
     MatListModule,
